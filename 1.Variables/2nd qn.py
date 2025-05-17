1c597beb6043f8476b1bee166c4276a3b7bfd9a9
Akhil S <akhilsenthil696@gmail.com>
2025-05-17 10:40:40 +0530

    Delete 1.Variables directory

--- 1.Variables/2nd qn.py
+++
@@ -1,3 +0,0 @@
-for=4    
-'''This will raise a SyntaxError
-The name for cant be assigned for a variable as for is a keyword used for looping'''
--- 1.Variables/3rd qn.py
+++
@@ -1,5 +0,0 @@
-P=10000
-R=5
-T=3
-SI=(P*R*T)/100
-print("Simple Interest: Rs.",SI)     
