b2d4ec43a6906cdfdf127e46d5782ff5c1fcd8ec
Akhil S <akhilsenthil696@gmail.com>
2025-05-17 10:40:59 +0530

    Delete 2.Numbers directory

--- 2.Numbers/2nd qn.py
+++
@@ -1,4 +0,0 @@
-def format_values(a,b):
-    print(f"The formatted values are {a} and {b}".format())
-    
-format_values(145,"o")
--- 2.Numbers/3rd qn.py
+++
@@ -1,6 +0,0 @@
-def speed_calculation(distance,time):
-    time_in_secs=time*60
-    speed=distance/time
-    return speed
-speed_calculated=speed_calculation(490,7)
-print("The required speed is ",speed_calculated)
